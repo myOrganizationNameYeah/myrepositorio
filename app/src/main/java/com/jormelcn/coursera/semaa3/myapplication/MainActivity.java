cee68b27bf6a867f80beeb391d7a755fee33af08
jormelcn <jormelcn@gmail.com>
2016-04-17 22:38:07 -0500

    mi primer commit

---
+++ app/src/main/java/com/jormelcn/coursera/semaa3/myapplication/MainActivity.java
@@ -0,0 +1,13 @@
+package com.jormelcn.coursera.semaa3.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
